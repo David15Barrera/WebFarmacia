190d11702a66e861ffcaf5951a17505c28e9edf9
David15Barrera <david1315barrera@gmail.com>
2024-02-23 23:21:51 -0600

    Starting Frontend

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'inicio', component: InicioComponent },
+//  { path: '', redirectTo: '/inicio', pathMatch: 'full' }, 
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
