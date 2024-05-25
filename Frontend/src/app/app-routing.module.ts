e0f054ad6f6fd12ffba63e569d463a0b77535961
David15Barrera <david1315barrera@gmail.com>
2024-05-24 23:07:37 -0600

    changes to the order of html components

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { InicioComponent } from './inicio/inicio.component';
 import { LoginComponent } from './login/login.component';
+import { InicioAdminComponent } from './admin/inicio-admin/inicio-admin.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'inicio', component: InicioComponent },
-//  { path: '', redirectTo: '/inicio', pathMatch: 'full' }, 
+  {path: 'admin/inicioAdmin', component: InicioAdminComponent },
+  { path: '', redirectTo: '/inicio', pathMatch: 'full' }, 
 
 ];
 
--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InicioComponent } from './inicio/inicio.component';
 import { LoginComponent } from './login/login.component';
+import { InicioAdminComponent } from './admin/inicio-admin/inicio-admin.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     InicioComponent,
-    LoginComponent
+    LoginComponent,
+    InicioAdminComponent
   ],
   imports: [
     BrowserModule,
